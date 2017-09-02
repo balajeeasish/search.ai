2e6fb18ddbbb7dc4f811163f8f452f2c594902a5
saiguy3 <saiatmakuri@yahoo.com>
2017-09-01 18:30:53 -0700

    TMB Data Query

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ results_table_app.use(express.static(__dirname + '/public'));
 
 //call Wit API with the SERVER_ACCESS_TOKEN
 const Wit = require('node-wit').Wit;
-const wit = new Wit({accessToken: 'KHTO5ZH3NCVM7GVHNTTIQCTBKCAID5HF'});
+const wit = new Wit({accessToken: 'GVBQOTI2MKXYIM265DOVIKII44BSWIFF'});
 
 //set up json-query
 var jsonQuery = require('json-query');
@@ -63,7 +63,7 @@ var nsp2 = results_table_io.of('/' + username);
 nsp2.on('connection', function(socket){
 	//default: show all patients
 	readContent(function (err, data) {
-		formatResponseTable(jsonQuery('[*]', { data: data }).value, function(message) {
+		formatResponseTable(jsonQuery('study.patients[*]', { data: data }).value, function(message) {
 			nsp2.emit('bot message', {msg: message});
 		});
 	});
@@ -77,11 +77,18 @@ nsp2.on('connection', function(socket){
 //parse JSON files for data in an async function
 var fs = require('fs');
 function readContent(callback) {
- 	fs.readFile('db/data.json', 'utf8', function (err, data) {
+ 	fs.readFile('db/study.json', 'utf8', function (err, data) {
     	if (err) throw err;
     	var obj = JSON.parse(data);
     	callback(null, obj);
   	});
+}
+function readTMBContent(callback) {
+	fs.readFile('db/Patient_TMB.json', 'utf8', function (err, data) {
+		if (err) throw err;
+    	var obj = JSON.parse(data);
+    	callback(null, obj);
+	});
 }
 
 //send bot messages by emitting 'bot message'
@@ -139,25 +146,29 @@ function createQuery(entities, keys) {
 //formats the result of the query in a readable fashion
 function formatResponse(result, callback) {
 	if (result.length > 0) {
-		var message = '';
-		var keys = ['Study', 'Visit', 'Originating ID', 'QC Reported Gender', 'Source Matcode', 'Container Matcode'];
-		for (var i = 0; i < result.length; i++) {
-			for (var j = 0; j < keys.length; j++) {
-				var key = keys[j];
-				//Capitalize the first letter of the keys
-				var formattedKey = key.charAt(0).toUpperCase() + key.slice(1);
-				//if a property is an array, print length of array instead of the array
-				if (Array.isArray(result[i][keys[j]])) {
-					var value = result[i][keys[j]].length;
-				} else {
-					var value = result[i][keys[j]];
+		readTMBContent(function (err, data) {	
+			var message = '';
+			//var keys = ['Study', 'Visit', 'Originating ID', 'QC Reported Gender', 'Source Matcode', 'Container Matcode'];
+			var keys = Object.keys(result[0]);
+			for (var i = 0; i < result.length; i++) {
+				for (var j = 0; j < keys.length; j++) {
+					var key = keys[j];
+					//Capitalize the first letter of the keys
+					var formattedKey = key.charAt(0).toUpperCase() + key.slice(1);
+					//if a property is an array, print length of array instead of the array
+					if (Array.isArray(result[i][keys[j]])) {
+						var value = result[i][keys[j]].length;
+					} else {
+						var value = result[i][keys[j]];
+					}
+					message += formattedKey + ': ' + value + '<br>';
 				}
-				message += formattedKey + ': ' + value + '<br>';
+				var queryResult = jsonQuery('[*patients = ' + result[i]['id'] + '][TMB]', { data: data }).value;
+				message += 'TMB: ' + queryResult + '<br><br>';
 			}
-			message += '<br>'
-		}
-		//return formatted message without the last '<br>'
-		callback(message.substring(0, message.length-4));
+			//return formatted message without the last '<br>'
+			callback(message.substring(0, message.length-4));
+		});
 	} else {
 		callback('');
 	}
@@ -166,30 +177,39 @@ function formatResponse(result, callback) {
 //formats the result of the query in a table fashion
 function formatResponseTable(result, callback) {
 	if (result.length > 0) {
-		var message = '<table id="table_msg">';
-		var keys = ['Study', 'Group', 'Visit', 'Originating ID', 'BioInventory Registration', 'USUBJID', 'Sample Status', 'QC Reported Gender',	'Source Matcode', 'Container Matcode', 'Concentration', 'Volume'];
-		//var keys = Object.keys(result[0]);
+		readTMBContent(function (err, data) {
+			var message = '<table id="table_msg">';
+			//var keys = ['Study', 'Group', 'Visit', 'Originating ID', 'BioInventory Registration', 'USUBJID', 'Sample Status', 'QC Reported Gender',	'Source Matcode', 'Container Matcode', 'Concentration', 'Volume'];
+			var keys = Object.keys(result[0]);
 
-		//go through the keys array to get all the table headings
-		message += '<tr><thead>';
-		for (var i = 0; i < keys.length; i++) {
-			var key = keys[i].toUpperCase();
-			message += '<th>' + key + '</th>';
-		}
-		message += '</tr></thead><tbody id="myTableBody">';
-		
-		//each following row contains the info of each patient in the results
-		for (var i = 0; i < result.length; i++) {
-			message += '<tr>';
-			for (var j = 0; j < keys.length; j++) {
-				var value = result[i][keys[j]];
-				message += '<td>' + value + '</td>';
+			//go through the keys array to get all the table headings
+			message += '<tr><thead>';
+			for (var i = 0; i < keys.length; i++) {
+				var key = keys[i].toUpperCase();
+				message += '<th>' + key + '</th>';
 			}
-			message += '</tr>';
-		}
-		
-		message += '</tbody></table>';
-		callback(message);
+			message += '<th>TMB</th>'
+			message += '</tr></thead><tbody id="myTableBody">';
+			
+			//each following row contains the info of each patient in the results
+			for (var i = 0; i < result.length; i++) {
+				message += '<tr>';
+				for (var j = 0; j < keys.length; j++) {
+					if (Array.isArray(result[i][keys[j]])) {
+						var value = result[i][keys[j]].length;
+					} else {
+						var value = result[i][keys[j]];
+					}
+					message += '<td>' + value + '</td>';
+				}
+				var queryResult = jsonQuery('[*patients = ' + result[i]['id'] + '][TMB]', { data: data }).value;
+				message += '<td>' + queryResult + '</td>';
+				message += '</tr>';
+			}
+			
+			message += '</tbody></table>';
+			callback(message);
+		});
 	} else {
 		callback('');
 	}
@@ -209,7 +229,7 @@ function handleMessage(question) {
       		switch (entities['intent'][0].value) {
 		        case 'show_patients':
           		readContent(function (err, data) {
-		            var queryResult = jsonQuery('[*' + createQuery(entities, keys) + ']', { data: data }).value;
+		            var queryResult = jsonQuery('study.patients[*' + createQuery(entities, keys) + ']', { data: data }).value;
             		formatResponse(queryResult, function(message) { 
 						send(message);
 					});
@@ -217,11 +237,29 @@ function handleMessage(question) {
           		break;
         		case 'show_studies':
           		readContent(function (err, data) {
-		            var queryResult = jsonQuery('[*' + createQuery(entities, keys) + ']', { data: data }).value;
+		            var queryResult = jsonQuery('study[*' + createQuery(entities, keys) + ']', { data: data }).value;
             		formatResponse(queryResult, function(message) { 
 						send(message);
 					});
           		});
+          		case 'show_patients_tmb':
+          		if (Array.isArray(entities['TMB'])) {
+	          		readTMBContent(function (err, data) {
+			            var queryResult = jsonQuery('[*TMB '+ entities['TMB'][0].value + ']', { data: data }).value;
+						readContent(function(err, data2) {
+				            var msg = '';
+				            for (var i = 0; i < queryResult.length; i++) {
+				            	var result = jsonQuery('study.patients[*id = '+ queryResult[i]['patients'] + ']', { data: data2 }).value;
+				            	formatResponse(result, function(message) { 
+									msg += message + '<br>';
+								});
+				            }
+				            formatResponse(result, function(message) {
+				            	send(msg.substring(0, msg.length-4));
+				            });
+				        });
+	          		});
+	          	}
           		break;
 	      	}
     	} else {
@@ -243,19 +281,41 @@ function handleResultsTableMessage(question) {
       		switch (entities['intent'][0].value) {
 		        case 'show_patients':
           		readContent(function (err, data) {
-		            var queryResult = jsonQuery('[*' + createQuery(entities, keys) + ']', { data: data }).value;
+		            var queryResult = jsonQuery('study.patients[*' + createQuery(entities, keys) + ']', { data: data }).value;
 					formatResponseTable(queryResult, function(message) {
 						tableSend(message);
+						console.log(message);
 					});
           		});
           		break;
         		case 'show_studies':
           		readContent(function (err, data) {
-		            var queryResult = jsonQuery('[*' + createQuery(entities, keys) + ']', { data: data }).value;
+		            var queryResult = jsonQuery('study[*' + createQuery(entities, keys) + ']', { data: data }).value;
             		formatResponseTable(queryResult, function(message) { 
 						tableSend(message);
 					});
           		});
+          		case 'show_patients_tmb':
+	          	if (Array.isArray(entities['TMB'])) {
+	          		readTMBContent(function (err, data) {
+			            var queryResult = jsonQuery('[*TMB '+ entities['TMB'][0].value + ']', { data: data }).value;
+						readContent(function(err, data2) {
+				            var msg = '<table id="table_msg"><tr><thead><th>ID</th><th>AGE</th><th>GENDER</th><th>EFFECTS</th><th>RESPONSE</th><th>TMB</th></tr></thead><tbody id="myTableBody">';
+				            for (var i = 0; i < queryResult.length; i++) {
+				            	var result = jsonQuery('study.patients[*id = '+ queryResult[i]['patients'] + ']', { data: data2 }).value;
+				            	formatResponseTable(result, function(message) { 
+									msg += message.substring(153, message.length-16);
+								});
+				            }
+				            formatResponseTable(result, function(message) {
+				            	tableSend(msg + '</tbody></table>');
+				            	//emit filters in query
+	        					var filterMsg = insertSpaces('TMB ' + entities['TMB'][0].value);
+	        					nsp2.emit('filter-criteria', {msg: filterMsg});
+				            });
+				        });
+	          		});
+	          	}
           		break;
 	      	}
     	} else {
